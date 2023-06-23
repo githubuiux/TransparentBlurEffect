016e10405f1de10e2222a31f2615ed583e4007c1
githubuiux <uiuxista@gmail.com>
2023-06-23 09:58:07 +0300

    Initial Commit

---
+++ TransparentBlurEffect/TransparentBlurEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentBlurEffectApp.swift
+//  TransparentBlurEffect
+//
+//  Created by Levent on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TransparentBlurEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
